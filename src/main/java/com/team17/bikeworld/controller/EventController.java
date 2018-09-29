c6c8ce64cc382703180bba962ed4c2dc19b0cf27
cvin59 <cvin59@gmail.com>
2018-09-29 10:33:36 +0700

    initial commit

--- src/main/java/com/team17/bikeworld/controller/EventController.java
+++ src/main/java/com/team17/bikeworld/controller/EventController.java
@@ -1,6 +1,7 @@
 package com.team17.bikeworld.controller;
 
 import com.team17.bikeworld.common.CoreConstant;
+import com.team17.bikeworld.model.Event;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(CoreConstant.API_EVENT)
-public class EventController {
+public class EventController{
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity getEvent() {
-        return new ResponseEntity("Hello World", HttpStatus.OK);
+    public ResponseEntity<Event> getEvent() {
+        return ResponseEntity.status(HttpStatus.OK).body(new Event());
     }
 }
