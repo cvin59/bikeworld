29ba2d9e727d681d0e210aa0d03134ae8a4884be
cvin59 <cvin59@gmail.com>
2018-10-09 01:40:59 +0700

    Update event api
    Crete proposal event entity + repo
    Config max upload size

---
+++ src/main/java/com/team17/bikeworld/entity/ProposalEvent.java
@@ -0,0 +1,77 @@
+package com.team17.bikeworld.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "proposal_event")
+public class ProposalEvent implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "name", length = 50)
+    private String name;
+    @Column(name = "image_url", length = 100)
+    private String imageUrl;
+
+    public ProposalEvent() {
+    }
+
+    public ProposalEvent(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Event)) {
+            return false;
+        }
+        ProposalEvent other = (ProposalEvent) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Event[ id=" + id + " ]";
+    }
+
+}
--- src/main/java/com/team17/bikeworld/service/EventService.java
+++ src/main/java/com/team17/bikeworld/service/EventService.java
@@ -1,16 +1,25 @@
 package com.team17.bikeworld.service;
 
-import com.team17.bikeworld.common.CoreConstant;
 import com.team17.bikeworld.entity.Event;
+import com.team17.bikeworld.entity.ProposalEvent;
 import com.team17.bikeworld.model.ConsumeEvent;
 import com.team17.bikeworld.repositories.EventRepository;
+import com.team17.bikeworld.repositories.ProposalEventRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,9 +28,12 @@ public class EventService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EventService.class);
     private final EventRepository eventRepository;
+    private final ProposalEventRepository proposalEventRepository;
+    private final Path rootLocation = Paths.get("src/main/resources/static/images").toAbsolutePath().normalize();
 
-    public EventService(EventRepository eventRepository) {
+    public EventService(EventRepository eventRepository, ProposalEventRepository proposalEventRepository) {
         this.eventRepository = eventRepository;
+        this.proposalEventRepository = proposalEventRepository;
     }
 
     public ResponseEntity<List<Event>> findEvents() {
@@ -56,11 +68,18 @@ public class EventService {
         return response;
     }
 
-    public ResponseEntity<Event> proposeEvent(ConsumeEvent consumeEvent) {
-        ResponseEntity<Event> response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<ProposalEvent> proposeEvent(ConsumeEvent consumeEvent, MultipartFile image) {
+        ResponseEntity<ProposalEvent> response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
         if (consumeEvent != null) {
             try {
-                Event event = eventRepository.save(initEvent(consumeEvent));
+                //xu ly luu hinh anh
+                String fileName = "hinhanh_" + consumeEvent.getName() + ".jpg";
+                Files.createDirectories(rootLocation);
+                Files.copy(image.getInputStream(), this.rootLocation.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+
+                consumeEvent.setImageUrl("/downloadFile/" + fileName);
+
+                ProposalEvent event = proposalEventRepository.save(initProposalEvent(consumeEvent));
                 response = ResponseEntity.status(HttpStatus.OK).body(event);
             } catch (Exception e) {
                 response = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -69,19 +88,32 @@ public class EventService {
         return response;
     }
 
-    private Event initEvent(ConsumeEvent consumeEvent) {
-        if (consumeEvent != null){
+    private ProposalEvent initProposalEvent(ConsumeEvent consumeEvent) {
+        if (consumeEvent != null) {
             try {
-                Event event = new Event();
+                ProposalEvent event = new ProposalEvent();
                 event.setName(consumeEvent.getName());
-                event.setStatus(CoreConstant.STATUS_EVENT_PENDING);
                 event.setImageUrl(consumeEvent.getImageUrl());
                 return event;
-            } catch (Exception e){
+            } catch (Exception e) {
                 LOGGER.error(e.getMessage());
             }
 
         }
         return null;
+    }
+
+    public Resource loadFileAsResource(String fileName) throws Exception {
+        try {
+            Path filePath = this.rootLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                throw new Exception("File not found " + fileName);
+            }
+        } catch (MalformedURLException ex) {
+            throw new Exception("File not found " + fileName, ex);
+        }
     }
 }
